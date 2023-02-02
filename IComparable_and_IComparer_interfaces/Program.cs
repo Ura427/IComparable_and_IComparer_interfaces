e40150b787b6745416568cc0084d3ab70fc8cd7e
Yura <urakost427@gmail.com>
2023-02-02 16:26:09 +0200

    Add project files.

---
+++ IComparable_and_IComparer_interfaces/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IComparable_and_IComparer_interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
